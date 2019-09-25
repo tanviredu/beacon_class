7334706edf6a4e7b6110bcd7f2c3b82ae1bde569
Tanvir Rahman <tanvirrahman@pop-os.localdomain>
2019-09-26 05:10:18 +0600

    added

--- functional_programming/calculator.php
+++ functional_programming/calculator.php
@@ -2,11 +2,54 @@
 
     class Calculator{
 
-        public function add(){
-            return true;
+        public function add($number1,$number2){
+            return $number1+$number2;
+        }
+        public function subtract($number1,$number2){
+            return $number1-$number2;
+        }
+        public function multiply($number1,$number2){
+            return $number1*$number2;
+        }
+        public function devide($number1,$number2){
+            return $number1/$number2;
+        }
+        public function sinf($number){
+            return sin($number);
+
+        }
+        public function cosf($number){
+            return cos($number);
+        }
+        public function tanf($number){
+            return tan($number);
+        }
+        public function logf($number){
+            return log($number);
+        }
+        public function sqrtroot($number){
+            return sqrt($number);
         }
 
     }
 
-    $calculator = new Calculator();
+    $cal = new Calculator();
+    echo $cal->add(1,2);
+    echo "</br>";
+    echo $cal->subtract(1,2);
+    echo "</br>";
+    echo $cal->multiply(1,2);
+    echo "</br>";
+    echo $cal->devide(1,2);
+    echo "</br>";
+    echo $cal->sinf(1.2);
+    echo "</br>";
+    echo $cal->cosf(1.2);
+    echo "</br>";
+    echo $cal->tanf(1.2);
+    echo "</br>";
+    echo $cal->logf(1.2);
+    echo "</br>";
+    echo $cal->sqrtroot(1.2);
+    echo "</br>";
 ?>
---
+++ functional_programming/first.php
@@ -0,0 +1,54 @@
+<html>
+    <head>
+        <title>Currency converter</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    </head>
+    <body>
+    <div class="jumbotron">
+        <div class="row">
+        
+            <!--div class='col-md-6'-->
+                <!--creating card -->
+                <div class="card"style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Basic Calculator</h5>
+                        <h6 class="card-subtitle mb-2 text-muted"></h6>
+                        <p class="card-text"> <!-- input takes here-->
+                            <form action="" method="POST">
+                                <label> Enter Ammount </label>
+                                <input type='text' name="one" placeholder="Enter Ammount"> 
+                                
+                               <p> <br/><input type="submit" class='btn btn-success'name="convert1" value="convert" > </p>
+                            </form>
+                        </p>
+                        <p class="alert alert-danger">
+                   
+                    </p>
+                    </div>
+                </div>
+            <!--/div-->
+            <!--div class='col-md-1'-->
+                <!--creating card -->
+                <div class="card"style="width: 20rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">Seientific calculator</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">BDT TO USD</h6>
+                        <p class="card-text"> <!-- input takes here-->
+                            <form action="" method="POST">
+                                <label> Enter Ammount </label>
+                                <input type='text' name="one" placeholder="Enter Ammount"> 
+                                
+                               <p> <br/><input type="submit" class='btn btn-success'name="convert1" value="convert" > </p>
+                            </form>
+                        </p>
+                        <p class="alert alert-danger">
+                   
+                    </p>
+                    </div>
+                </div>
+                
+        </div>
+    </div> 
+    </body>
+
+</html>
