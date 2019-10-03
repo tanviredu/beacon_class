d31f4d096699978a54e750479b4f6729407257c9
tanviredu <tanviredu2018@gmail.com>
2019-10-03 18:30:02 +0600

    added

--- oop/accessor.php
+++ oop/accessor.php
@@ -14,8 +14,25 @@
                             // if you dont add any id in the chield you can access the 
                             // parent by using this
         }
+
+
+        // the function have to be public if you want to
+        // acccess this propertise outside the class 
+        public function getID(){
+            return $this->id;
+        }
+
+        public function setID($id){
+            $this->id = $id;
+        }
     }
 
     $eee = new EEEstudent(1);
     //$eee->id = 2; // this will show an error in for the protected
+    // you can do enything with the protected value outside the class
+    // you can do it with a function
+    echo $eee->getID();
+    // if you use the private class you cant do anyhting with the
+    // other class not even inheritance 
+
 ?>
