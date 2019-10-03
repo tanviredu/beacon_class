deaf43e11800a75bf74a4c7c2bf94d96cec2ef87
tanviredu <tanviredu2018@gmail.com>
2019-10-03 18:11:49 +0600

    added

--- oop/second_oop.php
+++ oop/second_oop.php
@@ -4,7 +4,7 @@
 
         //protected $id;
         public $student;
-        public $name;
+        //public $name;
 
         public function __construct($id){
             $this->id = $id;
@@ -23,7 +23,24 @@
 
     class EEEstudent extends Student{
 
+        // create  additional properties
+        // you can add default value here
+        //public $dept="EEE";
+        // you can overwrite the whole thing if you add the same function
+        // if php found that you have a constructor in here it will no longer search 
+        // for parent class constructor
+        // so if you use the constructor here the parent constructor will no longer asked
+        // this is called method overwritng
+        
+        public function __construct(){
+            $this->dept ="EEE";
+        }
+
     }
-    $eeestudent = new EEEstudent('1234');\
+    $eeestudent = new EEEstudent('1234');
+    $eeestudent->dept = "EEE";
+
+    
+    
     var_dump($eeestudent);
 ?>
