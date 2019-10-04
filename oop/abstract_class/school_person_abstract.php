0d5370f2d3a18d4dc459f411c672d25d3fa3b080
root <root@pop-os.localdomain>
2019-10-04 08:32:31 +0600

    added

--- oop/abstract_class/school_person_abstract.php
+++ oop/abstract_class/school_person_abstract.php
@@ -5,8 +5,87 @@
         // student
         // Headmaster
         // librarian
-        //
+        
+        public  $name;
+        public  $gender;
+        public  $position;
+        
+
+        public function __construct($name,$gender,$position){
+            $this->name = $name;
+            $this->gender = $gender;
+            $this->position = $position;
+        }
+        abstract function display_information();
+
 
     }
 
+    class Teacher extends Person{
+        function __construct($name,$gender,$position){
+            
+            parent::__construct($name,$gender,$position);
+
+        }
+        public function display_information(){
+            if($this->gender=="male" && $this->position="teacher"){
+                echo "Mr. ".$this->name." is  teacher";
+
+            }else{
+                echo "Mrs. ".$this->name." is  teacher";
+            }
+            
+        }
+
+    }
+  
+
+
+    class Student extends Person{
+        function __construct($name,$gender,$position){
+            
+            parent::__construct($name,$gender,$position);
+
+        }
+        public function display_information(){
+            if($this->gender=="male" && $this->position="student"){
+                echo "Mr. ".$this->name." is  student";
+
+            }else{
+                echo "Mrs. ".$this->name." is  student";
+            }
+            
+        }
+
+    }
+
+
+
+
+    class Headmaster extends Person{
+        function __construct($name,$gender,$position){
+            
+            parent::__construct($name,$gender,$position);
+
+        }
+        public function display_information(){
+            if($this->gender=="male" && $this->position="headmaster"){
+                echo "Mr. ".$this->name." is  headmaster";
+
+            }else{
+                echo "Mrs. ".$this->name." is  headmistress";
+            }
+            
+        }
+
+    }
+    $student = new Student("tanvir",'male','student');
+    $student->display_information();
+
+
+    $teacher = new Teacher('abdul alim','male','teacher');
+    $teacher->display_information();
+
+    $head = new Headmaster('suchitra sen','female','headmaster');
+    $head->display_information();
 ?>
