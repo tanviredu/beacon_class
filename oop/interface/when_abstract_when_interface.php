141d4456143c7520504b2f2b5b6fd7dff5103d66
root <root@pop-os.localdomain>
2019-10-05 07:09:54 +0600

    added

--- oop/interface/when_abstract_when_interface.php
+++ oop/interface/when_abstract_when_interface.php
@@ -45,12 +45,41 @@
     1) is kind of (kar moto dekhte) [extends]
     2) is capable of (ki kaj kore)  [implements]
 
+    suppose there is a company and that company has Employee
+    for every employee there are attendance calculation and payroll calculation
+    1) there are management lavel employee and their work is contract sign and investment
+    2) there are  project magement level employee and their work is contract sign and client management
+    3) there are software developer lavel employee and their work is fixing Bug
+    4)there are quality assurance employee and their work is Bug report 
+    
+    
+    so the total class and interfae definition will be like this
 
-
-
+    
 */
 
+// base class
 
+abstract class Employee{
+    abstract public function calculateAttendence();
+    abstract public function generatePayrool();
+}
+
+class Management extends Employee implements Signcontract,Makeinvestment{
+
+}
+
+class projectManager extends Employee implements Signcontract,ManageClient{
+    
+}
+
+class SoftwareDeveloper extends Employee implements FixBug{
+    
+}
+
+class QualityAssurance extends Employee implements ReportBug{
+    
+}
 
 
 ?>
