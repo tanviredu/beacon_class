2fbff31e2e22c88b53da934e86a97b03086714db
tanvirrahman <ornobtanvir.git@gmail.com>
2019-10-13 15:25:31 +0600

    adding

--- pop_up_login_with_file_uploading/database.php
+++ pop_up_login_with_file_uploading/database.php
@@ -1,11 +1,11 @@
 <?php
-
+require_once('config.php');
 interface db_adapter{
-    public function open_connection();
-    public function close_connection();
-    public function confirm_query();
-    public function query();
-    public function fetch_array();
+    public function open_connection($server,$user,$pass,$name);
+    public function close_connection($connection);
+    public function confirm_query($result);
+    public function query($connection,$sql);
+    public function fetch_array($result_set);
 }
 
 class Database{
@@ -14,12 +14,45 @@ class Database{
         $this->adapter = $adapter;
 
     }
-    public function open_connection(){}
-    public function close_connection(){}
-    public function confirm_query(){}
-    public function query(){}
+    public function open_connection($server,$user,$pass,$name){
+        return $this->adapter->open_connection($server,$user,$pass,$name);
+    }
+    public function close_connection($connection){
+        return $this->adapter->close_connection($connection);
+    }
+    public function confirm_query($result){
+        return $this->adapter->confirm_query($result);
+    }
+    public function query($connection,$sql){
+
+    }
     public function fetch_array(){}
 
+}
+
+class Mysql implements db_adapter{
+    public function open_connection($server,$user,$pass,$name){
+        return mysqli_connect($server,$user,$pass,$name);
+    }
+    public function close_connection($connection){
+        if(isset($connection)){
+            mysqli_close($connection);
+            unset($connection);
+        }
+    }
+    public function confirm_query($result){
+        if($result){
+            return $result;
+        }else{
+            return false;
+        }
+    }
+    public function query($connetion,$sql){
+        return mysqli_query($connection,$sql);
+    }
+    public function fetch_array($result_set){
+        return mysqli_fetch_array($result_set);
+    }
 }
 
 
